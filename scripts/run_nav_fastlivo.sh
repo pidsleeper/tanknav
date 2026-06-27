8b217b3edc1d96a3c4c767e0e3a2f054c940af43
pidsleeper <2609795873@qq.com>
2026-06-27 16:38:47 +0800

    fix: source 顺序改为 tanknav_ws 先 fast_livo_ws 后，匹配桌面脚本的工作方式

--- scripts/run_nav_fastlivo.sh
+++ scripts/run_nav_fastlivo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eo pipefail
 source /opt/ros/humble/setup.bash
-source ~/fast_livo_ws/install/setup.bash
 source ~/tanknav_ws/install/setup.bash
+source ~/fast_livo_ws/install/setup.bash
 MAP_PATH=${1:-""}
 if [ -z "$MAP_PATH" ]; then
     echo "用法: $0 <地图路径.yaml>"
